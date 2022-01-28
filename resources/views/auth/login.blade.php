866c8e5604776ff6c7038377a550310e0f82c4ae
Michael Angelo De Leon <iammikedev@gmail.com>
2022-01-29 00:51:35 +0800

    change button text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Log ins') }}
                 </x-button>
             </div>
         </form>
