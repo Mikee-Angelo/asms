7d4c11175cdfb627dddb3ac1d2a555d614d49914
Michael Angelo De Leon <iammikedev@gmail.com>
2022-04-03 17:39:46 +0800

    fixed issue on medical and added some ui/ux enhancements

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> 
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
