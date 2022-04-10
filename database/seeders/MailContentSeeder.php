7e31b60c3d27cf46d29eb1fe044ce32e3ae8da80
Michael Angelo De Leon <iammikedev@gmail.com>
2022-04-10 18:38:10 +0800

    implemented custom mail management

---
+++ database/seeders/MailContentSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+//Models
+use App\Models\Mail;
+
+class MailContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Mail::create([
+            'tag' => 'Application',
+            'description' => "Thankyou for enrolling to Subic Bay Colleges (SBCI), Inc. Here's your student account",
+        ]);
+    }
+}
