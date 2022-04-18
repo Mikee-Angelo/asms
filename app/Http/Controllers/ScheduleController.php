b5f2b0ab3420d787d688e130f2bf70535f9dd8ec
Michael Angelo De Leon <iammikedev@gmail.com>
2022-04-18 10:26:11 +0800

    fixed issue on status column

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -42,9 +42,6 @@ class ScheduleController extends Controller
                     ->addColumn('count', function($row){ 
                         return $row->user->schedule->count() ?? 0;
                     })
-                    ->addColumn('status', function($row){ 
-                        return $row->user->schedule->status; 
-                    })
                     ->addColumn('action', function($row){
                         $btn = '<a href="'.route('schedule.show', ['schedule' => $row->id]).'" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">View</a>';
                         return $btn;
@@ -179,6 +176,9 @@ class ScheduleController extends Controller
                      ->addColumn('ends_at', function($row){ 
                         return Carbon::parse($row->ends_at)->format('g:i A');
                     })
+                    ->addColumn('status', function($row){ 
+                        return $row->status; 
+                    })
                     ->addColumn('action', function($row){
                         $btn = '';
                         if($row->status == 'draft') {
@@ -189,9 +189,6 @@ class ScheduleController extends Controller
                         
                         }
                         return $btn;
-                    })
-                    ->addColumn('status', function($row){ 
-                        return $row->status; 
                     })
                     ->rawColumns(['action'])
                     ->make(true);
