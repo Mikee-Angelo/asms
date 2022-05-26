c285fcf0eae2e94a50c6d5147db515b3006d6dff
Michael Angelo De Leon <iammikedev@gmail.com>
2022-05-26 16:35:50 +0800

    fixed issue on showing lec price

--- resources/views/components/miscellaneous-item-table.blade.php
+++ resources/views/components/miscellaneous-item-table.blade.php
@@ -26,7 +26,7 @@
          var table = $('#miscellaneous-item-datatable').DataTable({
              processing: true,
              serverSide: true,
-             ajax: "{{ route('miscellaneous.item.index', ['miscellaneou' => request()->application->fees->id ]) }}",
+             ajax: "{{ route('miscellaneous.item.index', ['miscellaneou' => request()->application->course_miscellaneous_id ]) }}",
              columns: [{
                      data: 'name',
                      name: 'name',
